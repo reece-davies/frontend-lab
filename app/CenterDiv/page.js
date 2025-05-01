52886d65060a47a2a6cb13356dafac6d063188b4
Reece Davies <153386893+reece-davies@users.noreply.github.com>
2025-05-01 18:15:47 +0000

    Added CenterDiv page

---
+++ app/CenterDiv/page.js
@@ -0,0 +1,13 @@
+
+export default function CenterDiv() {
+  return (
+    <div>
+      <h1 className="text-xl">CenterDiv</h1>
+
+      <div className="flex flex-col items-center justify-center space-y-4 min-h-[60vh] border-2 border-dotted"> 
+        <div className="w-[200px] h-[200px] bg-blue-200" />
+        <div className="w-[200px] h-[200px] bg-red-300" />
+      </div>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <h1 className="text-xl">Page list</h1>
       <ul>
-        <li><Link className="text-blue-600 underline hover:text-blue-800" href="/test">Itm1</Link></li>
+        <li><Link className="text-blue-600 underline hover:text-blue-800" href="/CenterDiv">CenterDiv</Link></li>
         <li><Link className="text-blue-600 underline hover:text-blue-800" href="/">Itm2</Link></li>
         <li><Link className="text-blue-600 underline hover:text-blue-800" href="/">Itm3</Link></li>
       </ul>
