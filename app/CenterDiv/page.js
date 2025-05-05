b574c50fc00b8f3606cbb2ab90427bfdf68edbbe
Reece Davies <153386893+reece-davies@users.noreply.github.com>
2025-05-05 13:20:19 +0000

    Removed title

--- app/CenterDiv/page.js
+++ app/CenterDiv/page.js
@@ -3,7 +3,7 @@ export default function CenterDiv() {
   return (
     <div className="flex flex-col items-center justify-center space-y-4 min-h-[60vh] border-2 border-dotted"> 
       <div className="w-[200px] h-[200px] bg-blue-200" />
-       <div className="w-[200px] h-[200px] bg-red-300" />
+      <div className="w-[200px] h-[200px] bg-red-300" />
     </div>
   );
 }
