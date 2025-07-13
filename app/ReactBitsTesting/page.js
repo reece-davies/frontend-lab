b03ee18ff1f117508624133c5ed1da943e68aace
Reece Davies <153386893+reece-davies@users.noreply.github.com>
2025-07-13 18:47:11 +0000

    installed DecryptedText and added to page

--- app/ReactBitsTesting/page.js
+++ app/ReactBitsTesting/page.js
@@ -1,8 +1,20 @@
+'use client'
+import DecryptedText from '@/components/DecryptedText/DecryptedText.jsx';
+//console.log('TEST:', require('@/components/DecryptedText/DecryptedText.jsx'));
 
 export default function ReactBitsTesting() {
   //flex flex-col items-center justify-center space-y-4 min-h-[60vh] border-2 border-dotted
   return (
-    <div className=""> 
+    <div className="">
+      <h2 className="text-xl font-bold">DecryptedText</h2>
+      <p>npx jsrepo add https://reactbits.dev/tailwind/TextAnimations/DecryptedText</p>
+      <DecryptedText
+        text="THIS IS THE DECRYPTED TEXT"
+        className="revealed"
+        speed="180"
+        animateOn="view"
+        revealDirection="start"
+        />
     </div>
   );
 }
